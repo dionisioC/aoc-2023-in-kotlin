4a183cac1b95daf03bc42044e189d607c695e9cf
Dionisio Cortés Fernández <cortesfernandezdionisio@gmail.com>
2023-12-08 17:59:36 +0100

    feat: Day 4 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -6,7 +6,8 @@ fun main() {
             var first = true
             val rawNumbers = line.substringAfter(':').trim()
             val winningNumbersAndNumbers = rawNumbers.split("|")
-            val winningNumbers = winningNumbersAndNumbers[0].trim().split(" ").filter { it.isNotBlank() }.map { it.toInt() }.toSet()
+            val winningNumbers =
+                winningNumbersAndNumbers[0].trim().split(" ").filter { it.isNotBlank() }.map { it.toInt() }.toSet()
             val numbers = winningNumbersAndNumbers[1].trim().split(" ").filter { it.isNotBlank() }.map { it.toInt() }
 
             for (number in numbers) {
@@ -27,7 +28,39 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var result = 0
+        val pointsPerCard = HashMap<Int, Int>()
+        val distributedPointsPerCard = HashMap<Int, Int>()
+        for (line in input) {
+            val cardPoints = ArrayList<Int>()
+            val cardNumber = line.substringBefore(':').trim().substringAfter(" ").trim().toInt()
+            val rawNumbers = line.substringAfter(':').trim()
+            val winningNumbersAndNumbers = rawNumbers.split("|")
+            val winningNumbers =
+                winningNumbersAndNumbers[0].trim().split(" ").filter { it.isNotBlank() }.map { it.toInt() }.toSet()
+            val numbers = winningNumbersAndNumbers[1].trim().split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+            distributedPointsPerCard[cardNumber] = distributedPointsPerCard.getOrDefault(cardNumber, 0) + 1
+            for (number in numbers) {
+                if (winningNumbers.contains(number)) {
+                    cardPoints.add(1)
+                }
+            }
+
+            for (j in 1..distributedPointsPerCard[cardNumber]!!) {
+                for (i in cardPoints.withIndex()) {
+                    distributedPointsPerCard[cardNumber + i.index + 1] =
+                        distributedPointsPerCard.getOrDefault(cardNumber + i.index + 1, 0) + 1
+                }
+            }
+
+            pointsPerCard[cardNumber] = cardPoints.sum()
+        }
+
+        for ((_, v) in distributedPointsPerCard) {
+            result += v
+        }
+
+        return result
     }
 
     // test if implementation meets criteria from the description, like:
